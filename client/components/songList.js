f400a91cdcdcc48d4e419b870176f1fd6e13bba9
Mannu Gupta <abhimanyu98986@gmail.com>
2018-09-17 15:20:24 +0530

    completed lecture 39

--- client/components/songList.js
+++ client/components/songList.js
@@ -3,12 +3,26 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
 class SongList extends Component {
+
+    renderSongs() {
+        return this.props.data.songs.map(song => {
+            // console.log(song.title);
+            return (
+                <li>
+                    { song.title }
+                </li>
+            );
+        });
+    }
+
     render() {
-        console.log(this.props);
-    return (
-        <div>
-            SongList
-        </div>
+    
+        if (this.props.data.loading) { return <div>Loading...</div>; }
+       
+        return (
+            <div>
+                { this.renderSongs() }
+            </div>
         );
     }
 }
