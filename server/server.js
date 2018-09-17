fc1a9112df9776414a38234e7ecaa9f84db2dd01
Mannu Gupta <abhimanyu98986@gmail.com>
2018-09-17 14:03:23 +0530

    completed lecture 38

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://theparadoxer02:mannu9898@ds255332.mlab.com:55332/lyricaldb-doxer';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
