c3d9a4bbfa765fcc0cca0d8ed9197cecc908ab84
Mannu Gupta <abhimanyu98986@gmail.com>
2018-09-19 14:53:55 +0530

    completed lecture 60

---
+++ client/components/LyricCreate.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class LyricsCreate extends Component {
+    render() {
+        return (
+            <form>
+                <label> Add a Lyrics </label>
+                <input />
+            </form>
+        );
+    }
+}
+
+export default LyricsCreate;
--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
 import fetchSong from '../queries/fetchSong';
-
+import LyricsCreate from './LyricCreate';
 
 class SongDetail extends Component {
     render() {
@@ -14,6 +14,7 @@ class SongDetail extends Component {
             <div>
                 <Link to="/"> Back </Link>
                 <h3> { song.title } </h3>
+                <LyricsCreate />
             </div>
         );
     }
