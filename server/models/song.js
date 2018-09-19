c6d4c77ce825eadd055dff0d306dd22f7ae870b1
Mannu Gupta <abhimanyu98986@gmail.com>
2018-09-19 16:23:03 +0530

    fixed lyrics not pushing bug

--- server/models/song.js
+++ server/models/song.js
@@ -11,7 +11,7 @@ const SongSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: 'lyric'
   }]
-});
+}, { usePushEach: true });
 
 SongSchema.statics.addLyric = function(id, content) {
   const Lyric = mongoose.model('lyric');
